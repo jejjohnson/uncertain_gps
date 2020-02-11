e82c1c6cbcb39051179a5c0987a2f3d585ff1985
jejjohnson <emanjohnson91@gmail.com>
2020-02-11 23:00:23 +0100

    Updated train model.

--- src/models/exact.py
+++ src/models/exact.py
@@ -14,9 +14,9 @@ from pyro.infer import TraceMeanField_ELBO, JitTraceMeanField_ELBO
 import tqdm
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from train_models import train
 
 pyro.enable_validation(True)  # can help with debugging
-pyro.set_rng_seed(0)
 
 
 class GPRegressor:
@@ -87,46 +87,6 @@ class GPRegressor:
                 return mean.detach().numpy(), var.detach().sqrt().numpy()
             else:
                 return mean.detach().numpy()
-
-
-def train(gpmodule, optimizer=None, loss_fn=None, retain_graph=None, num_steps=1000):
-    """
-    A helper to optimize parameters for a GP module.
-
-    :param ~pyro.contrib.gp.models.GPModel gpmodule: A GP module.
-    :param ~torch.optim.Optimizer optimizer: A PyTorch optimizer instance.
-        By default, we use Adam with ``lr=0.01``.
-    :param callable loss_fn: A loss function which takes inputs are
-        ``gpmodule.model``, ``gpmodule.guide``, and returns ELBO loss.
-        By default, ``loss_fn=TraceMeanField_ELBO().differentiable_loss``.
-    :param bool retain_graph: An optional flag of ``torch.autograd.backward``.
-    :param int num_steps: Number of steps to run SVI.
-    :returns: a list of losses during the training procedure
-    :rtype: list
-    """
-    optimizer = (
-        torch.optim.Adam(gpmodule.parameters(), lr=0.01)
-        if optimizer is None
-        else optimizer
-    )
-    # TODO: add support for JIT loss
-    loss_fn = TraceMeanField_ELBO().differentiable_loss if loss_fn is None else loss_fn
-
-    def closure():
-        optimizer.zero_grad()
-        loss = loss_fn(gpmodule.model, gpmodule.guide)
-        torch_backward(loss, retain_graph)
-        return loss
-
-    losses = []
-    with tqdm.trange(num_steps) as bar:
-        for epoch in bar:
-            loss = optimizer.step(closure)
-            losses.append(torch_item(loss))
-            # print statistics
-            postfix = dict(Loss=f"{torch_item(loss):.3f}")
-            bar.set_postfix(postfix)
-    return losses
 
 
 def main(args):
--- src/models/train_models.py
+++ src/models/train_models.py
@@ -1,114 +1,46 @@
 import sys
 import time
-
-import numpy as np
-
-from rbig import RBIG, RBIGKLD, RBIGMI
-
-sys.path.insert(0, "/home/emmanuel/code/rbig")
+import torch
+import tqdm
+from pyro.infer.util import torch_backward, torch_item
+from pyro.infer import TraceMeanField_ELBO
 
 
+def train(gpmodule, optimizer=None, loss_fn=None, retain_graph=None, num_steps=1000):
+    """
+    A helper to optimize parameters for a GP module.
 
-def run_rbig_models(X1, X2=None, measure="t", verbose=None, random_state=123):
+    :param ~pyro.contrib.gp.models.GPModel gpmodule: A GP module.
+    :param ~torch.optim.Optimizer optimizer: A PyTorch optimizer instance.
+        By default, we use Adam with ``lr=0.01``.
+    :param callable loss_fn: A loss function which takes inputs are
+        ``gpmodule.model``, ``gpmodule.guide``, and returns ELBO loss.
+        By default, ``loss_fn=TraceMeanField_ELBO().differentiable_loss``.
+    :param bool retain_graph: An optional flag of ``torch.autograd.backward``.
+    :param int num_steps: Number of steps to run SVI.
+    :returns: a list of losses during the training procedure
+    :rtype: list
+    """
+    optimizer = (
+        torch.optim.Adam(gpmodule.parameters(), lr=0.01)
+        if optimizer is None
+        else optimizer
+    )
+    # TODO: add support for JIT loss
+    loss_fn = TraceMeanField_ELBO().differentiable_loss if loss_fn is None else loss_fn
 
-    # RBIG Parameters
-    n_layers = 10000
-    rotation_type = "PCA"
-    zero_tolerance = 60
-    pdf_extension = 10
-    pdf_resolution = None
-    tolerance = None
+    def closure():
+        optimizer.zero_grad()
+        loss = loss_fn(gpmodule.model, gpmodule.guide)
+        torch_backward(loss, retain_graph)
+        return loss
 
-    if measure.lower() == "t" or measure.lower() == "h":
-
-        # RBIG MODEL 0
-        rbig_model = RBIG(
-            n_layers=n_layers,
-            rotation_type=rotation_type,
-            random_state=random_state,
-            zero_tolerance=zero_tolerance,
-            tolerance=tolerance,
-            pdf_extension=pdf_extension,
-            pdf_resolution=pdf_resolution,
-            verbose=verbose,
-        )
-
-        # fit model to the data
-        t0 = time.time()
-        rbig_model.fit(X1)
-        t1 = time.time() - t0
-        if verbose:
-            print(
-                f"Trained RBIG ({X1.shape[0]:,} points, {X1.shape[1]:,} dimensions): {t1:.3f} secs"
-            )
-
-        tc = rbig_model.mutual_information * np.log(2)
-        h = rbig_model.entropy(correction=True) * np.log(2)
-
-        if verbose:
-            print(f"TC: {tc:.3f}")
-            print(f"H: {h:.3f}")
-
-        return tc, h, t1
-
-    elif measure.lower() == "mi":
-        # RBIG MODEL 0
-        rbig_mi_model = RBIGMI(
-            n_layers=n_layers,
-            rotation_type=rotation_type,
-            random_state=random_state,
-            zero_tolerance=zero_tolerance,
-            tolerance=tolerance,
-            pdf_extension=pdf_extension,
-            pdf_resolution=pdf_resolution,
-            verbose=verbose,
-        )
-
-        # fit model to the data
-        t0 = time.time()
-        rbig_mi_model.fit(X1, X2)
-        t1 = time.time() - t0
-
-        if verbose:
-            print(
-                f"Trained RBIG1 MI ({X1.shape[0]:,} points, {X1.shape[1]:,} dimensions): {t1:.3f} secs"
-            )
-
-        mi = rbig_mi_model.mutual_information() * np.log(2)
-        if verbose:
-            print(f"MI: {mi:.3f}")
-
-        return mi, t1
-
-    elif measure.lower() == "kld":
-
-        rbig_kld_model = RBIGKLD(
-            n_layers=n_layers,
-            rotation_type=rotation_type,
-            random_state=random_state,
-            zero_tolerance=zero_tolerance,
-            tolerance=tolerance,
-            pdf_extension=pdf_extension,
-            pdf_resolution=pdf_resolution,
-            verbose=verbose,
-        )
-
-        # fit model to the data
-        t0 = time.time()
-        rbig_kld_model.fit(X1, X2)
-        t1 = time.time() - t0
-
-        if verbose:
-            print(
-                f"Trained RBIG KLD ({X1.shape[0]:,}, {X2.shape[0]:,} points): {t1:.3f} secs"
-            )
-
-        kld = rbig_kld_model.kld * np.log(2)
-        
-        if verbose:
-            print(f"KLD: {kld:.3f}")
-
-        return kld, t1
-
-    else:
-        raise ValueError(f"Unrecognized measure: {measure}.")
+    losses = []
+    with tqdm.trange(num_steps) as bar:
+        for epoch in bar:
+            loss = optimizer.step(closure)
+            losses.append(torch_item(loss))
+            # print statistics
+            postfix = dict(Loss=f"{torch_item(loss):.3f}")
+            bar.set_postfix(postfix)
+    return losses
